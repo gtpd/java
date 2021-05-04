8c635720fac69522d60adc1f527408cad1af26bc
Chris McErlain <cmcerlai4@gmail.com>
2021-05-04 17:26:39 -0400

    Comit 1

---
+++ clue.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class clue {
+    public static void main(String[] args){
+        System.out.println("you have done well, but this is not the end.");
+        System.out.print("Enter your name: ");
+        Scanner scan = new Scanner(System.in);
+        String b = scan.next();
+        System.out.println("the next clue is: ");
+        System.out.println(b + ", be sure to write down this passkey: GTPDRMBLNWRK1245");
+        System.out.println("The end of this challenge is near.");
+    }
+}
