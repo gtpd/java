3150a2baad68cba303944f6edb4dbabffb97f303
gtpd <83674503+gtpd@users.noreply.github.com>
2021-07-29 09:35:48 -0400

    Update clue.java

--- clue.java
+++ clue.java
@@ -5,8 +5,11 @@ public class clue {
         System.out.print("Enter your name: ");
         Scanner scan = new Scanner(System.in);
         String b = scan.next();
-        System.out.println("the next clue is: ");
+        System.out.println("the next clue is: https://tinyurl.com/58hxse78");
         System.out.println(b + ", be sure to write down this passkey: GTPDRMBLNWRK1245");
         System.out.println("The end of this challenge is near.");
+        //Don't read nothing to see here
+        //Bleep Bloop
+        throw new Exception("01010010 00110011 01000100 01000001 01000011 01010100 00110011 01000100");
     }
 }
